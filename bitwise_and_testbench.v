afca78cea23e1a01d6e70e45f2b55c2526c11bc1
Vedant-thereal <131479116+Vedant-thereal@users.noreply.github.com>
2023-07-04 18:29:49 +0530

    Add files via upload

---
+++ bitwise_and_testbench.v
@@ -0,0 +1,17 @@
+module testbench_bitand();
+    reg [7:0]a,b;
+    reg clk;
+    wire done;
+    wire [7:0]out;
+    bit_and w(.x(a),.y(b),.clk(clk),.done(done),.sum(out));
+    initial begin
+        #0 a=8'b00000111;
+        #0 b=8'b00000010;
+        #0 clk=1'b0;
+        repeat(10) begin
+         #5 clk=~clk;
+        end
+        #20 $display (out);
+        $finish;
+    end
+endmodule
